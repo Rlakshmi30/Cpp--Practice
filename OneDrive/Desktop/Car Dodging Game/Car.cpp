f48a90ee2ff1e50b142f9cb4a3d654c88453c9c4
Rlakshmi30 <r.lakshmi0048@gmail.com>
2026-05-12 14:27:54 +0530

    Updated

---
+++ OneDrive/Desktop/Car Dodging Game/Car.cpp
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace sf;
+using namespace std;
+const int WIDTH = 400;
+const int HEIGHT = 600;
